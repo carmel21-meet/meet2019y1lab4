cb684e9198d1430bd040ebd2e0a9572ece0902b0
carmel21-meet <carmel21@meet.mit.edu>
2019-07-24 09:54:23 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,23 @@
+fruit1 = input("What fruit do you have?")
+if fruit1 == "orange":
+    print("put it in bin 2")
+elif fruit1 == "apple":
+    print("put it in bin 1")
+else:
+    print("put it in bin 3")
+
+fruit2 = input("What fruit do you have?")
+if fruit2 == "orange":
+    print("put it in bin 2")
+elif fruit2 == "apple":
+    print("put it in bin 1")
+else:
+    print("put it in bin 3")
+
+fruit3 = input("What fruit do you have?")
+if fruit3 == "orange":
+    print("put it in bin 2")
+elif fruit3 == "apple":
+    print("put it in bin 1")
+else:
+    print("put it in bin 3")
